b6439c7acbfd94661dbad44038832a00d5d51b71
João Simões <nogravity00@outlook.com>
2017-08-12 19:13:44 +0100

    Helper interfaces for metadata

---
+++ src/SimpleSoft.Mediator.Abstractions/IHaveCreatedMeta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleSoft.Mediator
+{
+    /// <summary>
+    /// This instance has information about its creation
+    /// </summary>
+    public interface IHaveCreatedMeta
+    {
+        /// <summary>
+        /// The identifier for the user that created this instance
+        /// </summary>
+        string CreatedBy { get; }
+
+        /// <summary>
+        /// The date and time in which the instance was created
+        /// </summary>
+        DateTimeOffset CreatedOn { get; }
+    }
+}
---
+++ src/SimpleSoft.Mediator.Abstractions/IHaveIdentifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleSoft.Mediator
+{
+    /// <summary>
+    /// This instance has an unique identifier
+    /// </summary>
+    public interface IHaveIdentifier
+    {
+        /// <summary>
+        /// The unique identifier
+        /// </summary>
+        Guid Id { get; }
+    }
+}
