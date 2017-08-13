c49d46fa3c063ea97d0db62b288d5cfe1c003825
João Simões <nogravity00@outlook.com>
2017-08-13 18:39:16 +0100

    Included tests for DelegateHandlerFactory

---
+++ test/SimpleSoft.Mediator.Tests/DelegateHandlerFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace SimpleSoft.Mediator.Tests
+{
+    public class DelegateHandlerFactoryTests
+    {
+        [Fact]
+        public void GivenADelegateHandlerFactoryWhenPassingNullArgumentsThenAnArgumentNullExceptionMustBeThrown()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var factory = new DelegateHandlerFactory(null, type => null);
+                Assert.Null(factory);
+            });
+            Assert.NotNull(ex);
+
+            ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var factory = new DelegateHandlerFactory(type => null, null);
+                Assert.Null(factory);
+            });
+            Assert.NotNull(ex);
+        }
+
+        [Fact]
+        public void GivenADelegateHandlerFactoryWhenBuildingHandlersThenDelegatesMustBeInvoked()
+        {
+            var serviceInvoked = false;
+            var serviceCollectionInvoked = false;
+
+            var factory = new DelegateHandlerFactory(
+                type =>
+                {
+                    serviceInvoked = true;
+                    return null;
+                }, type =>
+                {
+                    serviceCollectionInvoked = true;
+                    return Enumerable.Empty<object>();
+                });
+
+            factory.BuildCommandHandlerFor<Command>();
+            Assert.True(serviceInvoked);
+
+            factory.BuildEventHandlersFor<Event>();
+            Assert.True(serviceCollectionInvoked);
+        }
+    }
+}
