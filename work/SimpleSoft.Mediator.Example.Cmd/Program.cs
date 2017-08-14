c65c5cb59679df339c1e9f313d4592a217bfe1f8
João Simões <nogravity00@outlook.com>
2017-08-14 17:39:06 +0100

    Added application logging

--- work/SimpleSoft.Mediator.Example.Cmd/Program.cs
+++ work/SimpleSoft.Mediator.Example.Cmd/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace SimpleSoft.Mediator.Example.Cmd
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleSoft.Mediator.Example.Cmd
 {
     public class Program
     {
+        private static readonly ILoggerFactory LoggerFactory;
+        private static readonly ILogger<Program> Logger;
+
+        static Program()
+        {
+            LoggerFactory = new LoggerFactory()
+                .AddConsole(LogLevel.Trace, true);
+            Logger = LoggerFactory.CreateLogger<Program>();
+        }
+
         public static void Main(string[] args)
         {
+            Logger.LogInformation("Application started...");
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Logger.LogCritical(0, e, "Fatal exception!");
+            }
+            finally
+            {
+                Logger.LogInformation("Application terminated. Press <enter> to exit...");
+                Console.ReadLine();
+            }
         }
     }
 }
