ab377027029962d78def4347c3f629cc9e9b253e
João Simões <nogravity00@outlook.com>
2017-08-18 02:37:26 +0100

    Release 1.0.0-rc03

--- tools/PackAllNuGets.ps1
+++ tools/PackAllNuGets.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 $assemblyVersion = "1.0.0"
-$assemblyFileVersion = "1.0.0.17226"
-$assemblyInformationalVersion = "1.0.0-rc02"
+$assemblyFileVersion = "1.0.0.17229"
+$assemblyInformationalVersion = "1.0.0-rc03"
 $nugetsDestinationPath = "..\nuget-builds\$($assemblyInformationalVersion)"
 
 Write-Host "Making a major cleanup..."
