6000187d13568a9b95d007fe4ff61d6f32e3a490
João Simões <nogravity00@outlook.com>
2017-08-19 19:32:59 +0100

    Tests for query handler not found exceptions

---
+++ test/SimpleSoft.Mediator.Tests/Mocks/MockQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleSoft.Mediator.Tests.Mocks
+{
+    public class MockQuery : Query<object>
+    {
+        public MockQuery()
+        {
+
+        }
+
+        public MockQuery(Guid id, DateTimeOffset createdOn, string createdBy)
+            : base(id, createdOn, createdBy)
+        {
+
+        }
+    }
+}
---
+++ test/SimpleSoft.Mediator.Tests/QueryHandlerNotFoundExceptionTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using SimpleSoft.Mediator.Tests.Mocks;
+using Xunit;
+
+namespace SimpleSoft.Mediator.Tests
+{
+    public class QueryHandlerNotFoundExceptionTests
+    {
+        [Fact]
+        public void GivenANullQueryWhenBuildingACommandHandlerNotFoundExceptionThenArgumentNullExceptionMustBeThrown()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var e = QueryHandlerNotFoundException.Build<IQuery<object>, object>(null);
+                Assert.Null(e);
+            });
+
+            Assert.NotNull(ex);
+        }
+
+        [Fact]
+        public void GivenAQueryWhenBuildingAQueryHandlerNotFoundExceptionThenAllPropertiesMustBeCorrect()
+        {
+            var query = new MockQuery();
+            var queryType = query.GetType();
+
+            var ex = QueryHandlerNotFoundException.Build<MockQuery, object>(query);
+
+            Assert.NotNull(ex.QueryData);
+            Assert.Same(query, ex.QueryData);
+
+            Assert.NotNull(ex.QueryType);
+            Assert.Same(queryType, ex.QueryType);
+
+            Assert.NotNull(ex.QueryName);
+            Assert.Equal(queryType.Name, ex.QueryName);
+
+            Assert.NotNull(ex.Message);
+            Assert.True(ex.Message.Contains(ex.QueryName));
+
+            Assert.NotNull(ex.Query<MockQuery, object>());
+            Assert.Throws<InvalidCastException>(() =>
+            {
+                ex.Query<IQuery<int>, int>();
+            });
+        }
+    }
+}
