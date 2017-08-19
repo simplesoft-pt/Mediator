ea9451398daaf99971c6b5315d4f34824a4980cd
João Simões <nogravity00@outlook.com>
2017-08-19 18:58:03 +0100

    #4 Added interfaces for queries support

---
+++ src/SimpleSoft.Mediator.Abstractions/IQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleSoft.Mediator
+{
+    /// <summary>
+    /// Represents a query handler
+    /// </summary>
+    /// <typeparam name="TQuery">The query type</typeparam>
+    /// <typeparam name="TResult">The result type</typeparam>
+    public interface IQueryHandler<in TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+        /// <summary>
+        /// Handles the given <see cref="IQuery{TResult}"/> and returns the
+        /// corresponding result.
+        /// </summary>
+        /// <param name="query">The query to handle</param>
+        /// <param name="ct">The cancellation token</param>
+        /// <returns>A task to be awaited for the result</returns>
+        Task<TResult> HandleAsync(TQuery query, CancellationToken ct = default(CancellationToken));
+    }
+}
---
+++ src/SimpleSoft.Mediator.Abstractions/Query.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SimpleSoft.Mediator
+{
+    /// <summary>
+    /// Represents a data query
+    /// </summary>
+    /// <typeparam name="TResult">The result type</typeparam>
+    public class Query<TResult> : IQuery<TResult>
+    {
+        /// <summary>
+        /// Creates a new instance using <see cref="Guid.NewGuid"/> and
+        /// <see cref="DateTimeOffset.Now"/> to set the default values.
+        /// </summary>
+        protected Query() : this(Guid.NewGuid(), DateTimeOffset.Now, null)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="id">The unique identifier</param>
+        /// <param name="createdOn">The date and time in which the instance was created</param>
+        /// <param name="createdBy">The identifier for the user that created this instance</param>
+        protected Query(Guid id, DateTimeOffset createdOn, string createdBy)
+        {
+            Id = id;
+            CreatedOn = createdOn;
+            CreatedBy = createdBy;
+        }
+
+        /// <inheritdoc />
+        public Guid Id { get; set; }
+
+        /// <inheritdoc />
+        public DateTimeOffset CreatedOn { get; set; }
+
+        /// <inheritdoc />
+        public string CreatedBy { get; set; }
+    }
+}
