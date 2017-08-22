834941dbbef22e37e0f9559866897f6e9351c9a7
João Simões <nogravity00@outlook.com>
2017-08-22 23:28:08 +0100

    Making sure the builders are never null

--- src/SimpleSoft.Mediator.Microsoft.Extensions.DependencyInjection/MediatorOptions.cs
+++ src/SimpleSoft.Mediator.Microsoft.Extensions.DependencyInjection/MediatorOptions.cs
@@ -8,12 +8,26 @@ namespace SimpleSoft.Mediator
     /// </summary>
     public class MediatorOptions
     {
+        private Func<IServiceProvider, IMediator> _mediatorBuilder =
+            provider => new Mediator(provider.GetRequiredService<IMediatorFactory>());
+
+        private Func<IServiceProvider, IMediatorFactory> _factoryBuilder =
+            provider => new MicrosoftMediatorFactory(provider);
+
         /// <summary>
         /// The function that builds the <see cref="IMediatorFactory"/> to be used. By default,
         /// it will build a <see cref="MicrosoftMediatorFactory"/> instance.
         /// </summary>
-        public Func<IServiceProvider, IMediatorFactory> FactoryBuilder { get; set; } =
-            provider => new MicrosoftMediatorFactory(provider);
+        public Func<IServiceProvider, IMediatorFactory> FactoryBuilder
+        {
+            get { return _factoryBuilder; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+                _factoryBuilder = value;
+            }
+        }
 
         /// <summary>
         /// The lifetime of the <see cref="IMediatorFactory"/> instance. By default,
@@ -25,8 +39,16 @@ namespace SimpleSoft.Mediator
         /// The function that builds the <see cref="IMediator"/> to be used. By default,
         /// it will build a <see cref="Mediator"/> instance.
         /// </summary>
-        public Func<IServiceProvider, IMediator> MediatorBuilder { get; set; } =
-            provider => new Mediator(provider.GetRequiredService<IMediatorFactory>());
+        public Func<IServiceProvider, IMediator> MediatorBuilder
+        {
+            get { return _mediatorBuilder; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+                _mediatorBuilder = value;
+            }
+        }
 
         /// <summary>
         /// The lifetime of the <see cref="IMediator"/> instance. By default,
