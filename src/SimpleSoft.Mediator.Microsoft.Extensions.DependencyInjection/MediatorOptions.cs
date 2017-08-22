638f3a55772acc328c365f3962f3d07cb009271b
João Simões <nogravity00@outlook.com>
2017-08-22 23:22:15 +0100

    #5 Created extensions to help with the services registration

---
+++ src/SimpleSoft.Mediator.Microsoft.Extensions.DependencyInjection/MediatorOptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleSoft.Mediator
+{
+    /// <summary>
+    /// The mediator registration options
+    /// </summary>
+    public class MediatorOptions
+    {
+        /// <summary>
+        /// The function that builds the <see cref="IMediatorFactory"/> to be used. By default,
+        /// it will build a <see cref="MicrosoftMediatorFactory"/> instance.
+        /// </summary>
+        public Func<IServiceProvider, IMediatorFactory> FactoryBuilder { get; set; } =
+            provider => new MicrosoftMediatorFactory(provider);
+
+        /// <summary>
+        /// The lifetime of the <see cref="IMediatorFactory"/> instance. By default,
+        /// the value is <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public ServiceLifetime FactoryLifetime { get; set; } = ServiceLifetime.Singleton;
+
+        /// <summary>
+        /// The function that builds the <see cref="IMediator"/> to be used. By default,
+        /// it will build a <see cref="Mediator"/> instance.
+        /// </summary>
+        public Func<IServiceProvider, IMediator> MediatorBuilder { get; set; } =
+            provider => new Mediator(provider.GetRequiredService<IMediatorFactory>());
+
+        /// <summary>
+        /// The lifetime of the <see cref="IMediator"/> instance. By default,
+        /// the value is <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public ServiceLifetime MediatorLifetime { get; set; } = ServiceLifetime.Singleton;
+    }
+}
---
+++ src/SimpleSoft.Mediator.Microsoft.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleSoft.Mediator
+{
+    /// <summary>
+    /// Extensions for the <see cref="IServiceCollection"/> interface.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Configures the mediator into the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        /// <param name="config">An optional configuration delegate</param>
+        /// <returns>The service collection after changes</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IServiceCollection AddMediator(
+            this IServiceCollection services, Action<MediatorOptions> config = null)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            var options = new MediatorOptions();
+            config?.Invoke(options);
+
+            services.Add(new ServiceDescriptor(
+                typeof(IMediator), options.MediatorBuilder, options.MediatorLifetime));
+            services.Add(new ServiceDescriptor(
+                typeof(IMediatorFactory), options.FactoryBuilder, options.FactoryLifetime));
+
+            return services;
+        }
+    }
+}
