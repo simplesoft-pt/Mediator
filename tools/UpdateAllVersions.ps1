23cf3ba7398095cf5c578f335762b71fd3dcd188
João Simões <nogravity00@outlook.com>
2017-09-14 00:07:37 +0100

    Created script to only change versions

---
+++ tools/UpdateAllVersions.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop"
+
+$assemblyVersion = "1.0.0"
+$assemblyFileVersion = "1.0.0.17235"
+$assemblyInformationalVersion = "1.0.0-rc05-final"
+
+Write-Host "Updating project versions..."
+
+$assemblyVersionText = "<AssemblyVersion>$($assemblyVersion)</AssemblyVersion>"
+$assemblyFileVersionText = "<FileVersion>$($assemblyFileVersion)</FileVersion>"
+$assemblyInformationalVersionText = "<VersionPrefix>$($assemblyInformationalVersion)</VersionPrefix>"
+
+Get-ChildItem -Path ".." -Recurse | 
+Where-Object {$_.FullName -like "*src*" -and $_.Name -like "*.csproj"} | 
+ForEach-Object {
+
+    $csprojFilePath = $_.FullName
+
+    Write-Host "Changing versions in file $($csprojFilePath)..."
+    $csprojContent = Get-Content $csprojFilePath -Raw -Encoding UTF8
+
+    $csprojContent = $csprojContent -replace '<AssemblyVersion>[0-9.]+</AssemblyVersion>', $assemblyVersionText
+    $csprojContent = $csprojContent -replace '<FileVersion>[0-9.]+</FileVersion>', $assemblyFileVersionText 
+    $csprojContent = $csprojContent -replace '<VersionPrefix>[0-9a-zA-Z.-]+</VersionPrefix>', $assemblyInformationalVersionText
+
+    $csprojContent | Set-Content $csprojFilePath -Encoding UTF8
+}
