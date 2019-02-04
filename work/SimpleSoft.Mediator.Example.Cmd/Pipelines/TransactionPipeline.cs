bde1f4e619bd94b0da1b1333e2922382d1644b07
João Simões <nogravity00@outlook.com>
2019-02-04 18:36:45 +0000

    Added example pipelines

---
+++ work/SimpleSoft.Mediator.Example.Cmd/Pipelines/TransactionPipeline.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleSoft.Mediator.Example.Cmd.Pipelines
+{
+    public class TransactionPipeline : Pipeline
+    {
+        private readonly ILogger<TransactionPipeline> _logger;
+        private readonly DbConnection _connection;
+
+        public TransactionPipeline(ILogger<TransactionPipeline> logger)
+        {
+            _logger = logger;
+            _connection = null; // DbContext, ISession... injected from container
+        }
+
+        public override async Task OnCommandAsync<TCommand>(Func<TCommand, CancellationToken, Task> next, TCommand cmd, CancellationToken ct)
+        {
+            _logger.LogDebug("Starting database transaction");
+
+            using (var tx = _connection?.BeginTransaction())
+            {
+                await next(cmd, ct);
+
+                _logger.LogDebug("Committing database transaction");
+                tx?.Commit();
+            }
+        }
+
+        public override async Task<TResult> OnCommandAsync<TCommand, TResult>(Func<TCommand, CancellationToken, Task<TResult>> next, TCommand cmd, CancellationToken ct)
+        {
+            _logger.LogDebug("Starting database transaction");
+
+            using (var tx = _connection?.BeginTransaction())
+            {
+                var result = await next(cmd, ct);
+
+                _logger.LogDebug("Committing database transaction");
+                tx?.Commit();
+
+                return result;
+            }
+        }
+    }
+}
--- work/SimpleSoft.Mediator.Example.Cmd/Program.cs
+++ work/SimpleSoft.Mediator.Example.Cmd/Program.cs
@@ -1,106 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using SimpleSoft.Hosting;
-using SimpleSoft.Hosting.Params;
+using SimpleSoft.Mediator.Example.Cmd.Pipelines;
 
 namespace SimpleSoft.Mediator.Example.Cmd
 {
     public class Program
     {
-        private static readonly CancellationTokenSource TokenSource;
-
-        static Program()
+        public static async Task Main(string[] args)
         {
-            TokenSource = new CancellationTokenSource();
-            Console.CancelKeyPress += (sender, eventArgs) =>
-            {
-                TokenSource.Cancel();
-                eventArgs.Cancel = true;
-            };
-        }
-
-        public static void Main(string[] args)
-        {
-            var loggerFactory = new LoggerFactory()
-                .AddConsole(LogLevel.Trace, true);
-
-            var logger = loggerFactory.CreateLogger<Program>();
-            logger.LogInformation("Application started...");
-            try
-            {
-                using (var hostBuilder = new HostBuilder()
-                    .UseLoggerFactory(loggerFactory)
-                    .UseStartup<ExampleStartup>())
+            await new HostBuilder()
+                .ConfigureLogging(b => b
+                    .AddConsole(o => o.IncludeScopes = true)
+                    .SetMinimumLevel(LogLevel.Trace))
+                .ConfigureServices((ctx, services) =>
                 {
-                    hostBuilder.RunHostAsync<ExampleHost>(TokenSource.Token)
-                        .ConfigureAwait(false).GetAwaiter().GetResult();
-                }
-            }
-            catch (TaskCanceledException)
-            {
-                logger.LogWarning("The code execution has been canceled.");
-            }
-            catch (Exception e)
-            {
-                logger.LogCritical(0, e, "Fatal exception!");
-            }
-            finally
-            {
-                logger.LogInformation("Application terminated. Press <enter> to exit...");
-                Console.ReadLine();
-            }
-        }
-
-        private class ExampleStartup : HostStartup
-        {
-            public override void ConfigureServiceCollection(IServiceCollectionHandlerParam param) =>
-                param.ServiceCollection
-                    .AddSingleton<IDictionary<Guid, User>>(s => new Dictionary<Guid, User>())
-                    .AddMediator();
-
-            public override IServiceProvider BuildServiceProvider(IServiceProviderBuilderParam param) =>
-                param.ServiceCollection.BuildServiceProvider(true);
-        }
-
-        private class ExampleHost : IHost
-        {
-            private readonly ILogger<ExampleHost> _logger;
-            private readonly IMediator _mediator;
-
-            public ExampleHost(ILogger<ExampleHost> logger, IMediator mediator)
-            {
-                _logger = logger;
-                _mediator = mediator;
-            }
-
-            public async Task RunAsync(CancellationToken ct)
-            {
-                for (var i = 0; i < 20; i++)
-                {
-                    _logger.LogDebug("Creating new user");
-                    var userId = await _mediator.SendAsync<RegisterUserCommand, Guid>(
-                        new RegisterUserCommand(Guid.NewGuid(), $"someuser{i:D2}@domain.com", "123456"), ct);
-
-                    _logger.LogDebug("Getting user '{userId}'", userId);
-                    var user = await _mediator.FetchAsync<UserByIdQuery, User>(
-                        new UserByIdQuery(Guid.NewGuid(), userId), ct);
-
-                    if (user == null)
+                    services.AddMediator(o =>
                     {
-                        _logger.LogDebug("User '{userId}' could not be found");
-                    }
-                    else
-                    {
-                        _logger.LogDebug("Changing password for user '{userId}'", userId);
-                        await _mediator.SendAsync(new ChangeUserPasswordCommand(
-                            Guid.NewGuid(), userId, "123456", "654321"), ct);
-                    }
-                }
-            }
+                        // pipeline order:
+                        //  mediator -> logging -> validation -> transaction -> handler
+                        o.AddPipeline<LoggingPipeline>(ServiceLifetime.Singleton);
+                        o.AddPipeline<ValidationPipeline>();
+                        o.AddPipeline<TransactionPipeline>();
+                    });
+                })
+                .RunConsoleAsync();
         }
     }
 }
